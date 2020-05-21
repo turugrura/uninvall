d9e466d5e4fabf735dc8efafff5912328dca28be
tongznr <apisitk10@gmail.com>
2020-05-21 23:39:37 +0700

    add security

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,27 @@
 import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
+import * as rateLimit from 'express-rate-limit'
+import * as helmet from 'helmet'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule)
+	const app = await NestFactory.create(AppModule, { cors: true })
+
 	app.useGlobalPipes(
 		new ValidationPipe({
 			transform: true,
 			whitelist: true,
 		}),
 	)
+	app.use(helmet())
+	// app.use(csurf())
+	app.use(
+		rateLimit({
+			windowMs: 15 * 60 * 1000, // 15 minutes
+			max: 100, // limit each IP to 100 requests per windowMs
+		}),
+	)
+
 	await app.listen(3000)
 }
 bootstrap()
