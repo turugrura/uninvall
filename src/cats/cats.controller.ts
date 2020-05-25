475847999bc42396c3a36184fcd4357f44f97462
tongznr <apisitk10@gmail.com>
2020-05-25 21:21:54 +0700

    update auth derorator

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,6 +1,5 @@
 import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common'
 import { Auth } from '../common/decorators/auth.decorator'
-// import { Auth } from '../common/decorators/auth.decorator'
 import { CatsService } from './cats.service'
 import { CreateCatDto } from './dto/create-cat.dto'
 import { UpdateCatDto } from './dto/update-cat.dto'
--- src/common/decorators/Auth.decorator.ts
+++ src/common/decorators/Auth.decorator.ts
@@ -1,10 +1,12 @@
-import { applyDecorators, SetMetadata, UseGuards } from '@nestjs/common'
+import { applyDecorators, UseGuards } from '@nestjs/common'
 import { AuthGuard } from '../guards/auth.guard'
 import { RolesGuard } from '../guards/roles.guard'
+import { Roles } from './roles.decorator'
 
 export function Auth(...roles: string[]): any {
 	return applyDecorators(
-		SetMetadata('roles', roles),
+		// SetMetadata('roles', roles),
+		Roles(...roles),
 		UseGuards(AuthGuard, RolesGuard),
 	)
 }
